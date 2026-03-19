f5cb7c357d96c33b0d533494a2be00d5e5ec8054
ankitKh-stack <ankitkharel18@gmail.com>
2026-03-19 04:01:32 -0400

    The elevator Check is DONEEEEEEEEEEgit add .

--- Assets/Scripts/SceneTrigger.cs
+++ Assets/Scripts/SceneTrigger.cs
@@ -5,8 +5,18 @@ using System.Collections;
 public class SceneTrigger : MonoBehaviour
 {
     [SerializeField] private LobbyManager lobbyManager;
-    public string sceneToLoad = "";
+    public string nextLevel = "";
+    public string loadLobby = "";
 
+    public enum elevatorType
+    {
+        anamoly,
+        noanamoly
+    }
+
+    public elevatorType eType;
+    
+    private LevelManager levelManager;
 
     void Awake()
     {
@@ -14,14 +24,42 @@ public class SceneTrigger : MonoBehaviour
         {
             lobbyManager = FindFirstObjectByType<LobbyManager>();
         }
+
+        levelManager = FindObjectOfType<LevelManager>();
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            bool hasAnomaly = false;
+
+            if (levelManager != null)
+            {
+                hasAnomaly = levelManager.HasAnomaly();
+            }
+
+            if (hasAnomaly && (eType == elevatorType.anamoly))
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+            else if (!hasAnomaly && (eType == elevatorType.anamoly))
+            {
+                SceneManager.LoadScene(loadLobby);
+            }
+            else if (!hasAnomaly && (eType == elevatorType.noanamoly))
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+            else if (hasAnomaly && (eType == elevatorType.noanamoly))
+            {
+                SceneManager.LoadScene(loadLobby);
+            }
+
+
         }
     }
+
 
 }
